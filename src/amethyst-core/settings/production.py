bfc024f9c116aba92d902aace711ae2286a905e7
Alex Bennett <me@alexben.net>
2018-09-07 13:11:01 -0500

    Changes to production settings

--- src/amethyst-core/settings/production.py
+++ src/amethyst-core/settings/production.py
@@ -5,4 +5,4 @@ from .default import *
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='^92l&5_l2f-ik5xlav!7*cat904fro-lmdd@0kgz@c*nxua3@p')
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alexben.net', 'www.alexben.net']
